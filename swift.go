8e692dd1676f895ef70c5994d51e0c0928b19e0f
Sylvain Baubeau <sbaubeau@redhat.com>
2015-11-13 14:48:57 +0100

    Allow querying SLO manifest

--- swift.go
+++ swift.go
@@ -1441,13 +1441,19 @@ var _ io.Seeker = &ObjectOpenFile{}
 // headers["Content-Type"] will give the content type if desired.
 func (c *Connection) ObjectOpen(container string, objectName string, checkHash bool, h Headers) (file *ObjectOpenFile, headers Headers, err error) {
 	var resp *http.Response
-	resp, headers, err = c.storage(RequestOpts{
+	opts := RequestOpts{
 		Container:  container,
 		ObjectName: objectName,
 		Operation:  "GET",
 		ErrorMap:   objectErrorMap,
 		Headers:    h,
-	})
+	}
+	if _, slo := h["X-Static-Large-Object"]; slo {
+		opts.Parameters = url.Values{}
+		opts.Parameters.Set("multipart-manifest", "get")
+		delete(h, "X-Static-Large-Object")
+	}
+	resp, headers, err = c.storage(opts)
 	if err != nil {
 		return
 	}
