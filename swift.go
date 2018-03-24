5abe5ae7b023cbf95b83b2e7fe417275a07c84d2
Yuri Per <yuri@acronis.com>
2018-03-24 15:22:09 +0300

    Fix file path encoding for bulk delete request
    File paths was overencoded (slashes replaced by %2F) and as result bulk delete fails with some Swift implementations

--- swift.go
+++ swift.go
@@ -1843,7 +1843,8 @@ type BulkDeleteResult struct {
 func (c *Connection) doBulkDelete(objects []string) (result BulkDeleteResult, err error) {
 	var buffer bytes.Buffer
 	for _, s := range objects {
-		buffer.WriteString(url.QueryEscape(s) + "\n")
+		u := url.URL{Path: s}
+		buffer.WriteString(u.String() + "\n")
 	}
 	resp, headers, err := c.storage(RequestOpts{
 		Operation:  "DELETE",
