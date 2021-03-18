52d2425d308d81a40150c14ff0126180da8ea5ec
Paul Collins <paul.collins@canonical.com>
2021-03-18 14:56:16 +1300

    factor out isLastPage

--- swift.go
+++ swift.go
@@ -947,6 +947,13 @@ func containersAllOpts(opts *ContainersOpts) *ContainersOpts {
 	return &newOpts
 }
 
+func (c *Connection) isLastPage(length int, limit int) bool {
+	if length < limit {
+		return true
+	}
+	return false
+}
+
 // ContainersAll is like Containers but it returns all the Containers
 //
 // It calls Containers multiple times using the Marker parameter
@@ -961,7 +968,7 @@ func (c *Connection) ContainersAll(ctx context.Context, opts *ContainersOpts) ([
 			return nil, err
 		}
 		containers = append(containers, newContainers...)
-		if len(newContainers) < opts.Limit {
+		if c.isLastPage(len(newContainers), opts.Limit) {
 			break
 		}
 		opts.Marker = newContainers[len(newContainers)-1].Name
@@ -983,7 +990,7 @@ func (c *Connection) ContainerNamesAll(ctx context.Context, opts *ContainersOpts
 			return nil, err
 		}
 		containers = append(containers, newContainers...)
-		if len(newContainers) < opts.Limit {
+		if c.isLastPage(len(newContainers), opts.Limit) {
 			break
 		}
 		opts.Marker = newContainers[len(newContainers)-1]
@@ -1173,7 +1180,7 @@ func (c *Connection) ObjectsWalk(ctx context.Context, container string, opts *Ob
 		default:
 			panic("Unknown type returned to ObjectsWalk")
 		}
-		if n < opts.Limit {
+		if c.isLastPage(n, opts.Limit) {
 			break
 		}
 		opts.Marker = last
